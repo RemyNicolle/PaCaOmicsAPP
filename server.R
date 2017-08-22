7b25bf2368e4b04b68ced80354654413d55145b8
RemyNicolle <remy.c.nicolle@gmail.com>
2017-08-22 15:17:39 +0200

    added methylation plots

--- server.R
+++ server.R
@@ -8,22 +8,17 @@ load("shinyData.RData")
 momcolors= c( basal="#ff7f00",classical="#377eb8",Outlier="#636363")
 tumostromcol=c(tumor="#148D4F",stroma="#89000B")
 
+#methdat,methdiff
 
-#H2M<<-cit.load("/datacit/00_DATABANKS/HumanMouseHomologs/H2M.RData")
-
-#"Tumor","Stroma","TumorVsStroma"
-# Mrnadiff,Mrna,Hrnadiff,Hrna,fullclassif,conorm,humanmousefac,StromaTumDiff
-
-#selectionmethod=list(totalReads=sum,SD=sd)
-
-L=list(
+RNAseqAllDat=list(
 Tumor=list(diff=Hrnadiff,exp=Hrna,cl=fullclassif,col=momcolors,genecol="GeneName"),
 Stroma=list(diff=Mrnadiff,exp=Mrna,cl=fullclassif,col=momcolors,genecol="HumanHomolog_Symbol"),
-TumorVsStroma=list(diff=StromaTumDiff,exp=conorm,cl=humanmousefac,col=tumostromcol,genecol="HumanGene"))
+TumorVsStroma=list(diff=StromaTumDiff,exp=conorm,cl=humanmousefac,col=tumostromcol,genecol="HumanGene")
+)
 
 
-genebyclass=function(gene,dataname){
-    DATA=L[[dataname]]
+RNAseqgeneplot=function(gene,dataname){
+    DATA=RNAseqAllDat[[dataname]]
     
     
     
@@ -60,7 +55,7 @@ genebyclass=function(gene,dataname){
   
   boxplot(as.numeric(DATA$exp[selectgeneEnsID,names(DATA$cl)])~DATA$cl,add=T,axes=F,outline=F,
   col=DATA$col,
-  sub=paste("log fold-change:",signif(DATA$diff[selectgeneEnsID,"logFC"],3),
+  sub=paste("Basal versus classical log fold-change:",signif(DATA$diff[selectgeneEnsID,"logFC"],3),
   "\nFDR:",signif(DATA$diff[selectgeneEnsID,"adj.P.Val"],3)))
   
   beeswarm(as.numeric(DATA$exp[selectgeneEnsID,names(DATA$cl)])~DATA$cl,
@@ -73,12 +68,108 @@ genebyclass=function(gene,dataname){
 }
 
 
+selectCpG=function(gene,type,group){
+
+if(gene==""| gene ==" " | is.null(gene)){
+ return(NULL)
+}else if(gene %in% methdiff[,"symbol"]){
+        selectgeneCPGID=rownames(methdiff)[which(methdiff[,"symbol"]==gene)]
+    }else if(tolower(gene) %in% rownames(methdiff)){
+        return(tolower(gene))
+    }else{
+        return(NULL)
+        
+    }
+    
+    
+    if(type!="any"){
+        
+        selectgeneCPGID=selectgeneCPGID[which(methdiff[selectgeneCPGID,"Type"]==type)]
+        
+    }
+    
+    if(group!="any"){
+        
+
+        selectgeneCPGID=selectgeneCPGID[which(methdiff[selectgeneCPGID,"GROUP"]==group)]
+    }
+    
+    if(length(selectgeneCPGID)==0){
+        return(NULL)
+    }else if(length(selectgeneCPGID)==1){
+            return(length(selectgeneCPGID)==0)
+    }else{
+        return(selectgeneCPGID[order(methdiff[selectgeneCPGID,"adj.P.Val"])])
+    }
+    
+    
+    
+    
+}
+
+Methgeneplot=function(cpgs,gene,type,group){
+    
+    
+    if(is.null(gene) |is.null(cpgs)){
+        plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+        text(x = 0.5, y = 0.5, paste("No CpG for",gene,"found with island type",
+        type,"and with",group,"position"),cex = 1.6, col = "black")
+        return(NULL)
+        
+        
+    }
+    
+        beeswarm(as.numeric(methdat[cpgs[1],names(fullclassif)])~fullclassif,
+    bty="l",pch=16,
+    xlab=NA,
+    ylab=paste("Methylation Beta-value (",cpgs[1],")"),
+    main=paste(gene,"(",type,",",group,")"))
+    
+    
+    boxplot(as.numeric(methdat[cpgs[1],names(fullclassif)])~fullclassif,
+    add=T,axes=F,outline=F,
+    col=momcolors,
+    sub=paste( "Basal versus classical FDR:",signif(methdiff[cpgs[1],"adj.P.Val"],3)))
+    
+    beeswarm(as.numeric(methdat[cpgs[1],names(fullclassif)])~fullclassif,
+    pch=16,
+    add=T,axes=F,
+    xlab=NA,
+    ylab=NA,
+    main=NA,cex=1.5)
+    
+}
+
+
+
+
 shinyServer(function(input, output) {
     
     
     
-    output$main_plot <- renderPlot({
-        genebyclass( gene=toupper(input$gene),input$data)
+    output$rnaseq_plot <- renderPlot({
+        RNAseqgeneplot( gene=toupper(input$gene),input$data)
         
     })
+    
+    
+    selectcpg=reactive({
+        selectCpG(toupper(input$methgene),input$island,input$group)
+
+    })
+    
+    output$methylation_plot <- renderPlot({
+        
+        Methgeneplot(selectcpg()
+                ,toupper(input$methgene),input$island,input$group)
+        
+    })
+    
+    output$cpglist <- renderTable({
+        df=methdiff[selectcpg(),c("symbol","Type","GROUP")]
+        colnames(df)=c("Gene symbol","Island status","gene position")
+        df=data.frame(CpG=rownames(df),df)
+        df
+    })
+
 })
--- ui.R
+++ ui.R
@@ -1,17 +1,64 @@
-library(shiny)
+#library(shiny)
 
 
-shinyUI(bootstrapPage(
+navbarPage("PaCaOmics",
 
+
+
+
+tabPanel("RNAseq",
+sidebarLayout(
+
+
+sidebarPanel(
 textInput(inputId="gene",
 value="NPC1L1",
 label="Enter gene symbol"
 ),
-
 selectInput(inputId="data",label="Select dataset:",
-choices=c("Tumor","Stroma","TumorVsStroma"),selected="Tumor"),
+choices=c("Tumor","Stroma","TumorVsStroma"),selected="Tumor")
+),
 
-plotOutput(outputId = "main_plot")
 
 
-))
+mainPanel(
+plotOutput("rnaseq_plot")
+)
+
+)),
+
+
+
+
+
+
+tabPanel("Methylation",
+sidebarLayout(
+
+
+sidebarPanel(
+textInput(inputId="methgene",
+value="NPC1L1",
+label="Enter gene symbol or CpG ID:"
+),
+selectInput(inputId="island",label="Select island status:",
+choices=c("any","Island","Shore","Shelf","non-CGI"),selected="any"),
+
+selectInput(inputId="group",label="Select position in gene:",
+choices=c("any","TSS1500",  "TSS200","1stExon","5'UTR",  "Body" ,"3'UTR"),selected="any"),
+
+tags$p("Other possible CpG:"),
+tableOutput("cpglist")
+),
+
+
+
+mainPanel(
+plotOutput("methylation_plot")
+)
+
+)))
+
+
+
+
